7792036d47310d22d1974474aba25888ff3ed303
jcscruggs <jaredscruggs@outlook.com>
2022-03-16 20:08:08 -0400

    Add project files.

---
+++ EmailLibrary/EmailClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmailLibrary
+{
+    public class EmailClient
+    {
+
+        public EmailClient()
+        {
+
+        }
+
+        public void connect()
+        {
+
+        }
+
+        public void send()
+        {
+
+        }
+
+
+
+    }
+}
