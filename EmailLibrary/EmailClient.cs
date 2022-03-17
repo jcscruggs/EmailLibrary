ab2f077b4e6572d53ce9e622be8587e7a11f1e69
jcscruggs <jaredscruggs@outlook.com>
2022-03-16 20:19:25 -0400

    added Initial methods

--- EmailLibrary/EmailClient.cs
+++ EmailLibrary/EmailClient.cs
@@ -1,23 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 
 namespace EmailLibrary
 {
     public class EmailClient
     {
-
-        public EmailClient()
+        private readonly ILogger<EmailClient> _logger;
+        public EmailClient(ILogger<EmailClient> logger = null)
         {
-
+            _logger = logger;
         }
 
         public void connect()
         {
-
+            _logger?.LogInformation("logging connection method in email client");
         }
 
         public void send()
         {
-
+            _logger?.LogInformation("logging send method in email client");
         }
 
 
